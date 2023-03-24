9305a3fea866c874bde728708ab855365555c6eb
tawheed78 <tawheed.chilwan55@gmail.com>
2023-03-24 17:49:56 +0530

    def sett

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -57,7 +57,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -16,14 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf.urls.static import static
-from django.conf import settings
-from django.views.static import serve
-from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 
-    url(r'^media/(?p<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?p<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]

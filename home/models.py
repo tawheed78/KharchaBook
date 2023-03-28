6d1f7fa98c291ae7dbf427a24051ab2687bd2dbd
tawheed78 <119650487+tawheed78@users.noreply.github.com>
2023-03-28 16:23:43 +0530

    Removed unnecessary code

--- home/models.py
+++ home/models.py
@@ -3,15 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
-# class Expense(models.Model):
-#     user = models.ForeignKey(User,default = 1, on_delete=models.CASCADE)
-#     profile = models.ForeignKey(Profile, )
-#     name = models.CharField(max_length=100)
-#     amount = models.CharField(max_length = 10 )
-#     date = models.DateField(default = now)
-    
-#     def __str__(self):
-#         return self.name
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
